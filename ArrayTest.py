3a1d925c1aadb1e2a5733a8892f7e5b7256bcad2
Onlybsen <biswajitsen@hotmail.com>
2020-08-04 22:14:18 +0800

    Create ArrayTest.py
    1st testing

---
+++ ArrayTest.py
@@ -0,0 +1,21 @@
+from array import *
+
+arr = array('i', [])
+n = int(input("Enter no. of array elements: "))
+for i in range(n):
+    val = int(input("Enter No.:"))
+    arr.append(val)
+
+print(arr)
+idx = 0
+v = int(input("Enter the value you want to search: "))
+for k in arr:
+    if k == v:
+        print("Index=",idx)
+        break
+    idx += 1
+else:
+    print("not found")
+
+print("Hello")
+print(arr.index(v)) #will cause error if v is not found in the array
