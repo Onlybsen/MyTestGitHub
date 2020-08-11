cf366e9d2c0a4a7d89048cc372da13b1a96392a4
Onlybsen <biswajitsen@hotmail.com>
2020-08-11 22:55:19 +0800

    Update FuncArguments.py

--- FuncArguments.py
+++ FuncArguments.py
@@ -13,4 +13,22 @@ def persdata(**kwdata): #keyworded variable length argument
     for i,j in kwdata.items():
         print(i,j)
 
-persdata(Name='Biswajit',dob='08/10/60', place='Kolkata',Phone=84984343)
+persdata(Name='Biswajit',dob='18/01/46', place='Singapore',Phone=84984343)
+
+
+def names():
+    i = 5
+    lt =  0
+    ge = 0
+    while i > 0:
+        name = input('Enter Name = ')
+        if len(name) < 5:
+            lt += 1
+        else:
+            ge += 1
+        i -= 1
+    return lt, ge
+
+lt, ge = names()
+print("Count of names LT 5 letters:",lt,"and GE 5 letters:",ge)
+print("Count of names lt 5 letters: {} and ge 5 letters: {}".format(lt, ge))
