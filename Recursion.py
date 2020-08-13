eb377d037e596ed040f518c8ba132be901d1fc9a
Onlybsen <biswajitsen@hotmail.com>
2020-08-13 23:16:59 +0800

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -1,7 +1,18 @@
 
 import sys
 
-print(sys.getrecursionlimit())
+#print(sys.getrecursionlimit())
 
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n-1)
 
+result = fact(5)
+print(result)
 
+#Use lambda or anonymous function
+val = (2,4,5,3,6,8,9,4)
+
+evens = list(filter(lambda n : n%2==0, val))
+print(evens)
