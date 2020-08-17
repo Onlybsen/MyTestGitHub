36406b1f30a61b64dbacf347d2f2bc48698f377e
Onlybsen <biswajitsen@hotmail.com>
2020-08-17 23:09:59 +0800

    Update Lambda.py

--- Lambda.py
+++ Lambda.py
@@ -4,7 +4,7 @@ import Recursion as aa
 def lamb():
     val = (2,4,5,3,6,8,9,4)
 
-    evens = list(filter(lambda n : n%2==0, val)) #anonymous function
+    evens = list(filter(lambda n: n%2==0, val)) #anonymous function
     print(evens)
     print(id(evens))    #memory address
     print(type(evens))  #data type
