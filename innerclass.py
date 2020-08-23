88247cda9058922dd20112d7e48dad07abe8373d
Onlybsen <biswajitsen@hotmail.com>
2020-08-23 18:22:47 +0800

    Update innerclass.py

--- innerclass.py
+++ innerclass.py
@@ -4,12 +4,12 @@ class student:
         self.name = name
         self.rollno = rollno
         #self.lap = self.laptop("Dell","i7","16gb")
-        self.lap = self.laptop("aa","bb","cc") #creating object inside the outer class 'student'
+        self.lap = self.laptop("Acer","Pentium III","4gb") #creating object inside the outer class 'student'
 
     def show(self):
         print(self.name,self.rollno)
 
-    class laptop:       #Inner class
+    class laptop:       #creating a class inside the class 'student'. Also called as Inner class
         def __init__(self,brand,cpu,ram):
             self.brand = brand
             self.cpu = cpu
@@ -18,9 +18,9 @@ class student:
 s1=student("Naveen",19)
 s2=student("Arora",21)
 
-lap1=student.laptop("Dell","i7","16gb") #creating object outside the class 'student'
-lap2=student.laptop("HP","i5","8gb")    #creating object outside the class 'student'
+print(s1.name,s1.rollno,s1.lap.brand,s1.lap.cpu,s1.lap.ram) #using object 'lap' created inside class 'student'
+
+lap1=student.laptop("Dell","i7","16gb") #creating 1st object outside the class 'student'
+lap2=student.laptop("HP","i5","8gb")    #creating 2nd object outside the class 'student'
 print(s1.name,s1.rollno,lap1.brand,lap1.cpu,lap1.ram)       #using object 'lap1' created outside class 'student'
 print(s2.name,s2.rollno,lap2.brand,lap2.cpu,lap2.ram)       #using object 'lap2' created outside class 'student'
-
-print(s1.name,s1.rollno,s1.lap.brand,s1.lap.cpu,s1.lap.ram) #using object 'lap' created inside class 'student'
