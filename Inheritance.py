4fe92bbbc11e4c69f058efccf5c5b6dd1a824c64
Onlybsen <biswajitsen@hotmail.com>
2020-09-06 18:15:06 +0800

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -3,20 +3,27 @@ class student:
     def __init__(self, name, rollno):
         self.name = name
         self.rollno = rollno
+        print("From student class:", self.name,self.rollno)
         #self.lap = self.laptop("Acer","Pentium III","4gb") #creating object inside the outer class 'student'
 
     def show1(self):
-        print(self.name,self.rollno)
-        print(s1.lap.show())
+        print("From show1:",self.name,self.rollno)
+        #print(s1.lap.show())
 
-class laptop(student):       #creating a class inside the class 'student'. Also called as Inner class
+class laptop(student):       #Inheritance
     def __init__(self,brand,cpu,ram):
+        super().__init__(brand,cpu)
         self.brand = brand
         self.cpu = cpu
         self.ram = ram
+        super().show1()
+
+    def show1(self):            #Method overloading
+        print("Method overloading of show1()")
 
     def show2(self):
-        print(self.brand,self.cpu,self.ram)
+        print("From show2:", self.brand,self.cpu,self.ram)
 
 l1 = laptop("AA","3","xx")
-print(l1.show2())
+l1.show2()
+l1.show1()
